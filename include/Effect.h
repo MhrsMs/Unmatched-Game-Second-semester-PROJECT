6a2ac96803d8643fbcd3ae4dc20d8f69f132eaa2
faezeghanaty <faezeghanaty@gmail.com>
2026-07-10 13:40:22 +0330

    adding effect class

---
+++ include/Effect.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "CardManager.h"
+#include "Hero.h"
+#include "MapManager.h"
+struct Data
+{
+	vector <Hero*> team;
+	vector <Hero*> target;
+	CardManager& cardsTeam;
+	CardManager& cardsTarget;
+	MapManager& mapManager;
+	Card& targetcard;
+	Hero& actor;
+	int& damage;
+};
+struct Complet_Needs
+{
+	vector <Card> optionalCard;
+	int location;
+	int number;
+	bool heroWin;
+	Hero& teamPerson;
+	Hero& targetPerson;
+};
+class Effects
+{
+public:
+
+	int apply_effect(int id, Data data, Complet_Needs complet_needs);
+	int rm(int min, int max);
+	void effect1(int a, Data data, Complet_Needs complet_needs);
+	void effect2(int a, Data data, Complet_Needs complet_needs);
+	void effect3(Data data, Complet_Needs complet_needs);
+	void effect4(Data data, Complet_Needs complet_needs);
+	void effect5(Data data, Complet_Needs complet_needs);
+	void effect6(Data data, Complet_Needs complet_needs);
+	void effect7(Data data, Complet_Needs complet_needs);
+	void effect8(Data data, Complet_Needs complet_needs);
+	void effect9(Data data, Complet_Needs complet_needs);
+	void effect10(Data data, Complet_Needs complet_needs);
+	void effect11(Data data, Complet_Needs complet_needs);
+	void effect12(Data data, Complet_Needs complet_needs);
+	void effect13(Data data, Complet_Needs complet_needs);
+};
+
---
+++ src/Effect.cpp
@@ -0,0 +1,237 @@
+#include "Effect.h"
+#include <random>
+
+int Effects::apply_effect(int id, Data data, Complet_Needs complet_needs)
+{
+	switch (id) //0=do nothing/ 1 = take on action /2 = move 2 cell /3 = move 3 cell /4 = farib/5 = show target hand
+	{
+	case 1:effect4(data, complet_needs); return 0;
+	case 2:effect11(data, complet_needs); return 1;
+	case 3:effect5(data, complet_needs); return 0;
+	case 4:effect3(data, complet_needs); return 2;
+	case 5:effect2(1, data, complet_needs); return 0;
+	case 6:effect2(4, data, complet_needs); return 0;
+	case 7:effect6(data, complet_needs); return 0;
+	case 8:effect2(2, data, complet_needs); return 0;
+	case 9:return 3;
+	case 10:return 4;
+	case 11:effect2(3, data, complet_needs); return 0;
+	case 12:effect1(1, data, complet_needs); return 0;
+	case 13:effect12(data, complet_needs); return 5;
+	case 14:effect7(data, complet_needs); return 0;
+	case 15:effect1(2, data, complet_needs); return 0;
+	case 18:return 3;
+	case 17:return 0;
+	case 19:effect13(data, complet_needs); return 0;
+	case 20:effect9(data, complet_needs); return 0;
+	case 21:effect10(data, complet_needs); return 0;
+	case 22:effect8(data, complet_needs); return 0;
+	case 23:return 4;
+	case 24:return 5;
+	case 25:return 0;
+	}
+}
+int Effects::rm(int min, int max)
+{
+	static std::mt19937
+		gen(std::random_device{}());
+	std::uniform_int_distribution<int>
+		dist(min, max);
+	return dist(gen);
+}
+void Effects::effect1(int a, Data data, Complet_Needs complet_needs)
+{
+	//1=bahre bardari 2 = emdad resani
+	if (a == 2)
+	{
+		data.mapManager.move(complet_needs.location, data.team[1]);
+	}
+	if (data.cardsTeam.can_deck_to_hand(1))
+	{
+		data.cardsTeam.deck_to_hand(1);
+	}
+	else
+	{
+		for (auto x : data.team)
+		{
+			x->decrease_HP(2);
+		}
+	}
+}
+void Effects::effect2(int b, Data data, Complet_Needs complet_needs)
+{
+	//1 = kamin 2 =form hayola 3=be cheshmanam negah kon 4 = atash khon
+	int id;
+	int increase;
+	if (b == 1)
+	{
+		if (!data.cardsTarget.hand.empty())
+		{
+			int a = rm(1, data.cardsTarget.hand.size());
+			int boost = data.cardsTarget.hand[a].get_boost();
+			data.cardsTarget.hand_to_null_card(data.cardsTarget.hand[a].get_id());
+			data.damage = boost + 2;
+		}
+	}
+	if (b == 2)
+	{
+		int a = complet_needs.optionalCard.size();
+		data.damage = a + 6;
+	}
+	if (b == 3)
+	{
+		int a = data.targetcard.get_boost();
+		data.damage = a + 1;
+	}
+	if (b == 4)
+	{
+		for (int i = 1; i < 4; i++)
+		{
+			if (data.team[i]->is_alive())
+			{
+				if (data.mapManager.is_same_zone(complet_needs.targetPerson.get_position(), data.team[i]->get_position()))
+				{
+					data.damage++;
+				}
+			}
+		}
+	}
+}
+
+
+void Effects::effect3(Data data, Complet_Needs complet_needs)
+{
+	//eqvay siri napazir
+	vector <Hero*> h = data.mapManager.nearby_heroes(complet_needs.targetPerson.get_position());
+	for (auto x : h)
+	{
+		if (x->get_name() == "SISTER")
+		{
+			complet_needs.targetPerson.decrease_HP(1);
+		}
+	}
+
+}
+void Effects::effect4(Data data, Complet_Needs complet_needs)
+{
+	//qosle khon 
+	data.actor.increase_HP(2);
+	bool found = false;
+	for (auto x : data.team)
+	{
+		if (!x->is_alive() && !found)
+		{
+			x->increase_HP(x->get_original_HP());
+			data.mapManager.move(complet_needs.location, x);
+			found = true;
+		}
+	}
+}
+
+void Effects::effect5(Data data, Complet_Needs complet_needs)
+{
+	//shekar kon
+	vector <Hero*> h = data.mapManager.nearby_heroes(data.actor.get_position());
+	for (auto x : h)
+	{
+		if (x->get_name() == "SHERLOCK" || x->get_name() == "DR.WATSON")
+		{
+			x->decrease_HP(1);
+			data.actor.increase_HP(1);
+		}
+	}
+}
+
+
+void Effects::effect6(Data data, Complet_Needs complet_needs)
+{
+	//teshnegi baray baqa
+	if (complet_needs.heroWin)
+	{
+		data.mapManager.move(complet_needs.location, data.team[1]);
+	}
+}
+void Effects::effect7(Data data, Complet_Needs complet_needs)
+{
+	//ostad taqir chehre
+
+	int hPosition = data.team[0]->get_position();
+	int tPosition = complet_needs.targetPerson.get_position();
+	data.mapManager.move(0, data.team[0]);
+	data.mapManager.move(hPosition, &complet_needs.targetPerson);
+	data.mapManager.move(tPosition, data.team[1]);
+	complet_needs.targetPerson.decrease_HP(1);
+}
+void Effects::effect8(Data data, Complet_Needs complet_needs)
+{
+	//amozesh hargez payan nadarad
+	if (complet_needs.heroWin)
+	{
+		if (data.cardsTarget.can_deck_to_hand(1))
+		{
+			data.cardsTarget.deck_to_hand(1);
+		}
+		else
+		{
+			for (auto x : data.target)
+			{
+				x->decrease_HP(2);
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 1; i++)
+		{
+			if (data.cardsTeam.can_deck_to_hand(1))
+			{
+				data.cardsTeam.deck_to_hand(1);
+			}
+			else
+			{
+				for (auto x : data.team)
+				{
+					x->decrease_HP(2);
+				}
+			}
+		}
+
+	}
+}
+
+void Effects::effect9(Data data, Complet_Needs complet_needs)
+{
+	//zed hamle
+	if (data.mapManager.is_adjacent(data.team[0]->get_position(), complet_needs.targetPerson.get_position()))
+	{
+		complet_needs.targetPerson.decrease_HP(2);
+	}
+}
+void Effects::effect10(Data data, Complet_Needs complet_needs)
+{
+	//noqtei sabet dar zamanei moteqaier
+	if ((data.team[0]->get_position()), (data.team[1]->get_position()))
+	{
+		data.team[0]->increase_HP(1);
+		data.team[1]->increase_HP(1);
+	}
+}
+
+void Effects::effect11(Data data, Complet_Needs complet_needs)
+{
+	//form meh
+	data.mapManager.move(complet_needs.location, data.team[0]);
+}
+
+void Effects::effect12(Data data, Complet_Needs complet_needs)
+{
+	//hazfe namomken ha
+	int id = complet_needs.optionalCard[0].get_id();
+	data.cardsTarget.hand_to_null_card(id);
+}
+
+void Effects::effect13(Data data, Complet_Needs complet_needs)
+{
+	//stentag sterategik
+	data.damage = data.targetcard.get_boost();
+}
